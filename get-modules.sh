18e31f9f584c8c992091313ca63e8a4bed6a313e
Joan Arbona <joan@gotes.org>
2020-05-10 16:15:47 +0200

    updated get-modules.sh

--- get-modules.sh
+++ get-modules.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ## Dependències OCA
-git clone https://github.com/OCA/l10n-spain.git -b 12.0 spain-addons/ # Descarregam els spain addons d'OCA
+temp_root=$(mktemp -d)
+git clone https://github.com/OCA/l10n-spain.git -b 12.0 $temp_root # Descarregam els spain addons d'OCA
 while read m; do # Descarregam totes les dependències
-	mkdir spain-addons/$m; 
-	git clone https://github.com/OCA/$m -b 12.0 spain-addons/$m;
-	rsync -rva spain-addons/$m/* spain-addons # cada repositori s'ha de moure al directori immediatament inferior ja que sinó no Odoo no els llegeix
-	rm -rf spain-addons/$m
-done<spain-addons/oca_dependencies.txt
+	tempd=$(mktemp -d)
+	git clone https://github.com/OCA/$m -b 12.0 $tempd;
+	rsync -rva $tempd/* spain-addons # cada repositori s'ha de moure al directori immediatament inferior ja que sinó no Odoo no els llegeix
+	rm -rf $tempd
+done<$temp_root/oca_dependencies.txt
+rsync -rva $temp_root/* spain-addons # cada repositori s'ha de moure al directori spain-addons ja que sinó no Odoo no els llegeix dins subdirectoris
+rm -rf $temp_root
 
 # Eliminam referències a impostos no definits
 sed -i -e "/l10n_es.account_tax_template_p_iva10_nd/d" -e "/l10n_es.account_tax_template_p_iva4_nd/d"  ./spain-addons/l10n_es_aeat_mod347/data/tax_code_map_mod347_data.xml
@@ -16,10 +19,9 @@ sed -i -e "/l10n_es.account_tax_template_p_iva10_nd/d" -e "/l10n_es.account_tax_
 sed -i -e "/l10n_es.account_tax_template_p_iva10_nd/d" -e "/l10n_es.account_tax_template_p_iva4_nd/d"  ./spain-addons/l10n_es_aeat_mod390/data/tax_code_map_mod390_data.xml
 
 # Dependències extra
-# echo "" >> extra_oca_dependencies.txt install more extra dependencies
 while read m; do # Descarregam totes les dependències
-        mkdir spain-addons/$m;
-        git clone https://github.com/OCA/$m -b 12.0 spain-addons/$m;
-        rsync -rva spain-addons/$m/* spain-addons # cada repositori s'ha de moure al directori immediatament inferior ja que sinó no Odoo no els llegeix
-        rm -rf spain-addons/$m
+	tempd=$(mktemp -d)
+        git clone https://github.com/OCA/$m -b 12.0 $tempd;
+        rsync -rva $tempd/* spain-addons # cada repositori s'ha de moure al directori immediatament inferior ja que sinó no Odoo no els llegeix
+        rm -rf $tempd
 done<extra_oca_dependencies.txt
